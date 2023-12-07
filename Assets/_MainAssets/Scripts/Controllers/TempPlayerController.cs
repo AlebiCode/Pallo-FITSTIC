804c3ce83e027e23588a24b8290d78d876843b69
AlebiGit <borg.ale01@gmail.com>
2023-12-07 14:24:45 +0100

    Scene up

--- Assets/_MainAssets/Scripts/Controllers/TempPlayerController.cs
+++ Assets/_MainAssets/Scripts/Controllers/TempPlayerController.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 namespace Controllers
 {
-
+    [RequireComponent(typeof(Rigidbody))]
     public class TempPlayerController : MonoBehaviour
     {
-        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private float speed = 1;
+
+        private Vector2 directionInput;
+
+        private void Update()
+        {
+            PlayerMovement();
+        }
+
+        private void PlayerMovement()
+        {
+            directionInput.x = Input.GetAxis("Horizontal");
+            directionInput.y = Input.GetAxis("Vertical");
+
+            rb.velocity += new Vector3(directionInput.x, 0, directionInput.y) * speed * Time.deltaTime;
+        }
 
     }
 
