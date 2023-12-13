29abc264e7d84e83eba5f4f875ec420d1e104049
daljen88 <davidecaso182@gmail.com>
2023-12-13 16:36:59 +0100

    mod dangers logic wip

--- Assets/_MainAssets/Scripts/Danger.cs
+++ Assets/_MainAssets/Scripts/Danger.cs
@@ -1,5 +1,8 @@
+using Controllers;
+using DavideCTest;
 using System.Collections;
 using System.Collections.Generic;
+using LorenzoCastelli;
 using UnityEngine;
 
 public class Danger : MonoBehaviour
@@ -12,39 +15,83 @@ public class Danger : MonoBehaviour
     [SerializeField] private bool isMovable;
     [SerializeField] private bool isActive;
 
+
+    [SerializeField] private float explosionRadius = 2f;
+
     // Quando la palla colpisce questo oggetto si attiva effetto diverso a seconda del tipo di pericolo (distinzione in base al colore)
+
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetType() == typeof(CapsuleCollider))
+        {
+            if (collision.gameObject.layer == 6)
+            {
+                DangerBehavior();
+            }
+            if (collision.gameObject.layer == 7)
+            {
+                //aggiungi logica palla contro trappola
+            }
+        }
+    }
+
     private void DangerBehavior()
     {
         if (isMovable)
         {
             // Attiva la possibilità di spostare il Danger tramite la fisica
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.isKinematic = true;
+            Rigidbody rb = this.GetComponent<Rigidbody>();
+
+            rb.isKinematic = false;
         }
         if (isActive)
         {
-            if (isRed)
+            var playersHit = Physics.OverlapSphere(this.transform.position, 2f, 1 << 6);
+
+            if (playersHit.Length > 0)
             {
-                // Comportamento del RedDanger
-                // Porta a zero la vita del personaggio vicino
-            }
-            if (isPurple)
-            {
-                // Comportamento del PurpleDanger
-                // Diminuisce la stamina di valore X
-                // Respinge il personaggio vicino
-            }
-            if (isYellow)
-            {
-                // Comportamento del YellowDanger
-                // Diminuisce la stamina di valore X del personaggio vicino
+
+                if (isRed)
+                {
+                    foreach (var player in playersHit )
+                    {
+                        KillPlayer(player.gameObject.GetComponentInChildren<TempPlayerController>());
+
+                    }
+
+                    //prima di distruzione esegui VFX and SFX
+
+                    Destroy(this.gameObject);
+
+                    // Comportamento del RedDanger
+                    // Porta a zero la vita del personaggio vicino
+                }
+                if (isPurple)
+                {
+                    foreach (var player in playersHit)
+                    {
+                        player.gameObject.GetComponent<p>
+                    }
+
+                    // Comportamento del PurpleDanger
+                    // Diminuisce la stamina di valore X
+                    // Respinge il personaggio vicino
+                }
+                if (isYellow)
+                {
+                    // Comportamento del YellowDanger
+                    // Diminuisce la stamina di valore X del personaggio vicino
+                }
             }
         }
     }
 
-    private void Explosion()
+    private void KillPlayer(TempPlayerController player)
     {
-
+        Debug.Log("player is killed!");
+        //rimuovi destroy e togli una vita
+        Destroy(player.gameObject);
     }
 
     private void StaminaDecrese()
