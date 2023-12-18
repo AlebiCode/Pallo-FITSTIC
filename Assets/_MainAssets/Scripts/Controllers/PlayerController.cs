b87d363b2f3991a05700ff91d44dc5255d5836ae
EnricoTed <enrico.tedaldi@fitstic-edu.com>
2023-12-18 16:37:45 +0100

    PlayerController wip
    renamed PlayerController to PlayerControllerOld
    remaking PlayerController to work with unity events through Player Input component on Player

--- Assets/_MainAssets/Scripts/Controllers/PlayerController.cs
+++ Assets/_MainAssets/Scripts/Controllers/PlayerController.cs
@@ -1,81 +1,105 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Enrico
+namespace Controllers
 {
-	public class PlayerController : MonoBehaviour
-	{
-		InputManager input;
-		bool isInitialized = false;
-		bool isMoving = false;
-		Vector2 moveVector;
-		private Rigidbody myRigidbody;
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(Rigidbody))]
 
-		private void Awake()
+    public class PlayerController : MonoBehaviour
+    {
+        private CharacterController controller;
+        private Vector3 playerVelocity;
+        private bool groundedPlayer;
+        [SerializeField] private float playerSpeed = 1f; //tradotto
+        private Vector2 movementInput = Vector2.zero;
+        private Vector3 mousePosition;
+        private bool dodged = false;
+        private PalloController heldPallo;
+
+        private void Start()
+        {
+            controller = gameObject.GetComponent<CharacterController>();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            movementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnRotation(InputAction.CallbackContext context)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
+            {
+                mousePosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            }
+        }
+
+        public void OnThrow(InputAction.CallbackContext context)
 		{
-			Init();
-		}
-
-		void Init()
-		{
-			if (!isInitialized)
-			{
-				input = new InputManager();
-				isInitialized = true;
-				myRigidbody = GetComponent<Rigidbody>();
-			}
-		}
-
-		private void OnEnable()
-		{
-			input.Enable();
-			input.Gameplay.Move.started += OnMoveStarted;
-			input.Gameplay.Move.performed += OnMovePerformed;
-			input.Gameplay.Move.canceled += OnMoveCanceled;
-			//oppure input.Gameplay.Move.canceled += context => OnMove(context.ReadValue<Vector2>());
-		}
-
-		private void OnDisable()
-		{
-			input.Disable();
-			input.Gameplay.Move.started += OnMoveStarted;
-			input.Gameplay.Move.performed += OnMovePerformed;
-			input.Gameplay.Move.canceled += OnMoveCanceled;
-		}
-
-		private void Update()
-		{
-			if (isMoving)
-			{
-				myRigidbody.AddForce(new Vector3(moveVector.x, 0, moveVector.y) * 5f);
-				//transform.position += (Vector3)moveVector * Time.deltaTime * 5;
-			}
-		}
-
-		private void OnMoveStarted(InputAction.CallbackContext callbackContext)
-		{
-			isMoving = true;
-
-			Debug.Log("Move started: " + moveVector);
-		}
-
-		private void OnMovePerformed(InputAction.CallbackContext callbackContext)
-		{
-			moveVector = callbackContext.ReadValue<Vector2>();
-			Debug.Log("Move performed: " + moveVector);
 
 		}
 
-		private void OnMoveCanceled(InputAction.CallbackContext callbackContext)
-		{
-			isMoving = false;
-			moveVector = Vector2.zero;
-			Vector2 direction = callbackContext.ReadValue<Vector2>();
+        public void OnParry(InputAction.CallbackContext context)
+        {
 
-			Debug.Log("Move cancelled: " + moveVector);
+        }
 
-		}
-	}
+        public void OnDodge(InputAction.CallbackContext context)
+        {
+            dodged = context.action.triggered;
+        }
+
+        //TODO trasformare in unityEvent
+        public void KillPlayer()
+        {
+            Debug.Log("player is killed!");
+            //TODO rimuovi destroy e togli una vita
+            Destroy(this.gameObject);
+        }
+
+        void Update()
+        {
+            UpdateMovement();
+
+            UpdateRotation();
+
+            UpdateThrow();
+
+            UpdateParry();
+
+            UpdateDodge();
+        }
+
+        private void UpdateMovement()
+        {
+            //groundedPlayer = controller.isGrounded; //TODO forse utile per quando viene spinto fuori mappa
+            if (playerVelocity.y < 0 /*TODO && !stoVenendoSpinto*/)
+                playerVelocity.y = 0f;
+
+            Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+            if (transform.position.y < 2)
+                controller.Move(move * Time.deltaTime * playerSpeed);
+        }
+
+        private void UpdateRotation()
+        {
+            //TODO gestire anche da gamepad (if keyboard -> mouseposition?)
+            transform.LookAt(mousePosition, Vector3.up);
+        }
+
+        private void UpdateThrow()
+        {
+            
+        }
+        private void UpdateParry()
+        {
+            
+        }
+        private void UpdateDodge()
+        {
+            
+        }
+    }
 }
