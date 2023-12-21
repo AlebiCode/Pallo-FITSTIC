60f690617aca3d33517fc7567a86d89d78c1a0cc
Tezca <vittoriovernassa@gmail.com>
2023-12-21 15:51:43 +0100

    21/12 Upd, see detail
    - Added Characters Art
    - Basic UI for out of gameplay menus
    - Added external plugin for Grid Layout Group (usato invece di quello di unity)

---
+++ Assets/_Testing/Vittorio/Script/PalloSceneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace vittorio {
+    public class PalloSceneManager : MonoBehaviour {
+
+        public static PalloSceneManager Instance { get; private set; }
+
+        private void Awake() {
+            
+            if(Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void LoadScene(int sceneID) {
+            SceneManager.LoadScene(sceneID);
+        }
+
+    }
+}
