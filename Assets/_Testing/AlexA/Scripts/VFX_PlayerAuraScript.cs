492b1b493b680332caa302df3ad77d8d07b3e1d1
AlexCrisis <alexanghel1203@Gmail.com>
2024-01-11 13:58:52 +0100

    Almost finished VFX_PlayerAura Implementation

--- Assets/_Testing/AlexA/Scripts/VFX_PlayerAuraScript.cs
+++ Assets/_Testing/AlexA/Scripts/VFX_PlayerAuraScript.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StateMachine;
+using DG.Tweening;
 
 public class VFX_PlayerAuraScript : MonoBehaviour
 {
-    [SerializeField] private Material lv0Material;
-    [SerializeField] private Material lv1Material;
-    [SerializeField] private Material lv2Material;
-    [SerializeField] private Material lv3Material;
+    [SerializeField] private List<Material> materials;
     
     private GameObject vfx_PlayerAuraCapsule;
     private GameObject vfx_PlayerAuraBase;
 
+    private Player myPlayer;
+
     private float maxSize;
     private float minSize;
     private float currentSize;
+    private bool isCharging;
 
     private float CurrentSize 
     {
@@ -34,13 +36,71 @@ public class VFX_PlayerAuraScript : MonoBehaviour
         }
     }
 
-    private void OnChargingStarted() 
+    private void OnDisable()
     {
-        vfx_PlayerAuraBase.SetActive(true);
+        myPlayer.stateMachine.throww.OnEnter -= OnChargingStarted;
+        myPlayer.stateMachine.throww.OnExit -= OnChargingCancelled;
+    }
+
+    private void Update()
+    {
+        OnChargingUpdate();
+    }
+
+    private void Init()
+    {
+        myPlayer = gameObject.GetComponentInParent<Player>();
+        myPlayer.stateMachine.throww.OnEnter += OnChargingStarted;
+        myPlayer.stateMachine.throww.OnExit += OnChargingCancelled;
+        vfx_PlayerAuraCapsule = transform.Find("PlayerAuraCapsule").gameObject;
+        vfx_PlayerAuraBase = transform.Find("PlayerAuraBase").gameObject;
+    }
+
+    private void CheckThrowLevel(int level)
+    {
+        switch (level) 
+        {
+            case 0:
+                vfx_PlayerAuraCapsule.GetComponent<ParticleSystemRenderer>().material = materials[0];
+                vfx_PlayerAuraBase.GetComponent<ParticleSystemRenderer>().material = materials[0];
+                break;
+            case 1:
+                vfx_PlayerAuraCapsule.GetComponent<ParticleSystemRenderer>().material = materials[1];
+                vfx_PlayerAuraBase.GetComponent<ParticleSystemRenderer>().material = materials[1];
+                break;
+            case 2:
+                vfx_PlayerAuraCapsule.GetComponent<ParticleSystemRenderer>().material = materials[2];
+                vfx_PlayerAuraBase.GetComponent<ParticleSystemRenderer>().material = materials[2];
+                break;
+            case 3:
+                vfx_PlayerAuraCapsule.GetComponent<ParticleSystemRenderer>().material = materials[3];
+                vfx_PlayerAuraBase.GetComponent<ParticleSystemRenderer>().material = materials[3];
+                break;
+        }
+    }
+
+    private void OnChargingStarted(Player owner) 
+    {
+        /*int throwLevel = player.heldPallo.speedTier;
+         *CheckThrowLevel(throwLevel);*/
         vfx_PlayerAuraCapsule.SetActive(true);
+        vfx_PlayerAuraBase.SetActive(true);
+        isCharging = true;
     }
     
     private void OnChargingUpdate() 
     {
+        if (isCharging) 
+        { 
+            vfx_PlayerAuraCapsule.transform.DOScale(maxSize, myPlayer.maxChargeTime);
+        }
+    }
+
+    private void OnChargingCancelled(Player owner) 
+    {
+        isCharging = false;
+        vfx_PlayerAuraCapsule.SetActive(false);
+        vfx_PlayerAuraBase.SetActive(false);
+        vfx_PlayerAuraCapsule.transform.DOScale(minSize, 0f);
     }
 }
