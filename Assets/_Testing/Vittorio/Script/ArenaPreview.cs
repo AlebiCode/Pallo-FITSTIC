d806fe5501b444d38aa1483649b0db415ed89507
Tezca <vittoriovernassa@gmail.com>
2024-02-28 16:43:30 +0100

    UI Update

--- Assets/_Testing/Vittorio/Script/ArenaPreview.cs
+++ Assets/_Testing/Vittorio/Script/ArenaPreview.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArenaPreview : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Image image;
+    [SerializeField] Text text;
 }
--- Assets/_Testing/Vittorio/Script/ArenaSelection.cs
+++ Assets/_Testing/Vittorio/Script/ArenaSelection.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class ArenaSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] ArenaPreview mainPreview;
+    [SerializeField] List<ArenaPortrait> arenaPortraitList;
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    private void Awake() {
         
     }
 }
